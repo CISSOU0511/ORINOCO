86111d9862405db99b9534f1d613f4b2676a816d
cecile gaubert <primaelswann@gmail.com>
2020-08-26 11:19:09 +0200

    initialisation oricono

---
+++ JWDP5/front/index.js
@@ -0,0 +1,30 @@
+fetch("http://localhost:3000/api/teddies")
+.then(reponse => reponse.json())
+.then(reponse => {
+    teddy(reponse)
+})
+
+function teddy(teddies){
+    console.log(teddies)
+    const teddyDiv = document.getElementById("teddy")
+    teddies.forEach(element => {
+        const article = document.createElement("article")
+        const paragraphe = document.createElement("p")
+        const image = document.createElement("img")
+        image.classList.add("")
+        const prix = document.createElement("p")
+        const bouton = document.createElement("button")
+        const lien = document.createElement("a")
+        lien.textContent = "ajouter au panier"
+        lien.href = "produit.html?id="+element._id
+        paragraphe.textContent = element.name
+        image.src = element.imageUrl
+        prix.textContent = element.price
+        bouton.appendChild(lien)        
+        article.appendChild(paragraphe)
+        article.appendChild(image)
+        article.appendChild(prix)
+        article.appendChild(bouton) 
+        teddyDiv.appendChild(article)
+    });
+}
