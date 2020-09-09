ed6b0e6548d157fcc55027f52ac862cfa9ae9671
cecile gaubert <primaelswann@gmail.com>
2020-09-09 22:38:06 +0200

    structuration page panier

---
+++ front/panier.js
@@ -0,0 +1,28 @@
+const url = window.location.search;
+const urlObject = new URLSearchParams(url);
+const id = urlObject.get("id");
+console.log(id)
+
+
+fetch("http://localhost:3000/api/teddies/" + id)
+.then(reponse => reponse.json())
+.then(reponse => {
+    teddy(reponse)
+})
+
+function teddy(element){    
+    const teddyDiv = document.getElementById("ours")
+    const article = document.createElement("article")
+    const paragraphe = document.createElement("p")
+    const image = document.createElement("img")
+    image.classList.add("image")                   
+    const prix = document.createElement("p") 
+    paragraphe.textContent = element.name
+    image.src = element.imageUrl
+    prix.textContent = element.price              
+    article.appendChild(paragraphe)
+    article.appendChild(image)
+    article.appendChild(prix)         
+    teddyDiv.appendChild(article)
+
+}
